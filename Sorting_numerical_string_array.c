7cd20cc33ce54032562ba98a0a8a7d6e9d06688e
Sankaran-S <31531389+Sankaran-S@users.noreply.github.com>
2018-03-07 20:19:04 +0530

    Add files via upload

---
+++ Sorting_numerical_string_array.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+    //decleration
+	char stringArray[10][10];
+	int n,a[10],i,j,temp,num,negativeStatus,len;
+	//input
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%s",stringArray[i]);
+	}
+	//numeric conversion
+	for(i=0;i<n;i++)
+	{
+	    num=0;
+	    negativeStatus=0;
+	    len=strlen(stringArray[i]);
+	    j=0;
+	    if(stringArray[i][0]=='-')
+	    {
+	        negativeStatus=1;
+	        j=1;
+	    }
+	    while(j<len)
+	    {
+	        num=num*10+(stringArray[i][j]-'0');
+	        j++;
+	    }
+	    if(negativeStatus==1)
+	    {
+	        num=num*-1;
+	    }
+	    a[i]=num;
+	}
+	//sorting
+	for(i=0;i<n-1;i++)
+	{
+	    for(j=i+1;j<n;j++)
+	    {
+	        if(a[j]<a[i])
+	        {
+	            temp=a[i];
+	            a[i]=a[j];
+	            a[j]=temp;
+	        }
+	    }
+	}
+	//output
+	for(i=0;i<n;i++)
+	{
+	    printf("%d ",a[i]);
+	}
+	return 0;
+}
+
