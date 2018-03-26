f3a505ca1125c7d141c6a99e6dd728d829e0f668
Sankaran-S <31531389+Sankaran-S@users.noreply.github.com>
2018-03-26 21:14:08 +0530

    Update NumberOfOne.c

--- NumberOfOne.c
+++ NumberOfOne.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
-
-
 int main(void) 
-{
-	
-int a,count=0;
-	
-scanf("%d",&a);
-	
-while(a>0)
-	
-	{
-	    
-	count=count+(a&1);
-	    
-	a=a>>1;
-	
-	}
-	
-printf("%d",count);
-	
+{	
+int a,count=0;	
+scanf("%d",&a);	
+while(a>0)	
+	{	    
+	count=count+(a&1);	    
+	a=a>>1;	
+	}	
+printf("%d",count);	
 return 0;
-
 }
