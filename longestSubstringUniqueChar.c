e198963dc1fb96155ec35b18c5394cbf7d289d9a
Sankaran-S <31531389+Sankaran-S@users.noreply.github.com>
2018-04-01 15:24:59 +0530

    Update longestSubstringUniqueChar.c

--- longestSubstringUniqueChar.c
+++ longestSubstringUniqueChar.c
@@ -3,29 +3,29 @@
 
 void substrings(char s[20])
 {
-    char sub[20],ans[20]="";
+	char sub[20],ans[20]="";
 	int a[127]={0},j=0,i,n;
-    n=strlen(s);
+	n=strlen(s);
 	for(i=0;i<n;i++)
 	{
-	    if(a[(int)s[i]]!=0)
-	    {
-	        if(j>=strlen(ans))
-	        {
-	            sub[j]='\0';
-	            strcpy(ans,sub);
-	        }
-	        j=0;
-	        memset(a,0,sizeof(a));
-	    }
-	    a[(int)s[i]]=1;
-	    sub[j]=s[i];
-	    j++;
+		if(a[(int)s[i]]!=0)
+		{
+	        	if(j>=strlen(ans))
+	        	{
+	            		sub[j]='\0';
+	            		strcpy(ans,sub);
+	        	}
+	        	j=0;
+	        	memset(a,0,sizeof(a));
+	    	}
+	    	a[(int)s[i]]=1;
+	    	sub[j]=s[i];
+	    	j++;
 	}
 	if(j>strlen(ans))
 	{
-	    sub[j]='\0';
-	    strcpy(ans,sub);
+	    	sub[j]='\0';
+	    	strcpy(ans,sub);
 	}
 	printf("%s",ans);
 }
